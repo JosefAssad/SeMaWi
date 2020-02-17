8b72c1bef382aee12e832bd764fc36fa42ed8774
Josef Assad <josef@josefassad.com>
2020-02-17 18:08:34 +0100

    speed up loading of the semawi data model
    If 50+ minutes load time is speeding up...
    
    We've downgraded mysql; version 8 was taking twice as long, and I see a lot of
    stories in github and on mediawiki of strange issues. So 5.7 looks to be the one
    to stick on for now.
    
    We're also splitting up dump importing and link rebuilding. It still takes a
    long time but there's results in the actual wiki a lot faster.

--- dockerfiles/semawi/scripts/loaddata.sh
+++ dockerfiles/semawi/scripts/loaddata.sh
@@ -1,5 +1,6 @@
 /bin/bash
 
 /usr/bin/php /var/www/wiki/maintenance/update.php --quick && \
-    /usr/bin/php /var/www/wiki/maintenance/importDump.php < /tmp/struktur.xml && \
+    /usr/bin/php /var/www/wiki/maintenance/importDump.php --no-updates < /tmp/struktur.xml && \
+    /usr/bin/php /var/www/wiki/maintenance/rebuildall.php && \
     /usr/bin/php /var/www/wiki/maintenance/runJobs.php
